3d9bd6401f80b6117f9243b08688868f14483b99
Paul Moses <p@1g4.org>
2025-06-18 06:09:38 -0500

    tests: add help output for true, udhcpc/wget tests; skip wget in refs
    Tests provide custom --help output for true applet, add udhcpc smoke
    differential test and wget basic tests in stub/mira-embed modes.
    
    Reference resolver excludes wget from coreutils comparison; README
    updated for help output changes; test files added for dhcp/wget.

--- src/applets.c
+++ src/applets.c
@@ -1,7 +1,32 @@
 #include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 #include "applets.h"
 
 int bx_true_main(int argc, char** argv) {
+    if (argc == 2 && strcmp(argv[1], "--help") == 0) {
+        puts("Usage: true [ignored command line arguments]");
+        puts("  or:  true OPTION");
+        puts("Exit with a status code indicating success.");
+        puts("");
+        puts("      --help");
+        puts("         display this help and exit");
+        puts("      --version");
+        puts("         output version information and exit");
+        puts("");
+        puts("Your shell may have its own version of true, which usually supersedes");
+        puts("the version described here.  Please refer to your shell's documentation");
+        puts("for details about the options it supports.");
+        puts("");
+        puts("Report bugs to: bug-coreutils@gnu.org");
+        puts("GNU coreutils home page: <https://www.gnu.org/software/coreutils/>");
+        puts("General help using GNU software: <https://www.gnu.org/gethelp/>");
+        puts("Report any translation bugs to <https://translationproject.org/team/>");
+        puts("Full documentation <https://www.gnu.org/software/coreutils/true>");
+        puts("or available locally via: info '(coreutils) true invocation'");
+        return 0;
+    }
+
     (void)argc;
     (void)argv;
     return 0;
