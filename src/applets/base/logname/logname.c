2281f80d21a2efdde127f7f139676eaf383cfe46
Paul Moses <p@1g4.org>
2026-05-22 13:37:32 -0500

    applets/base/logname: adopt bx_logname_parse_options and diag context
    The applet now parses --help and --version via bx_logname_parse_options
    returning booleans, reports unrecognized options and extra operands
    through diagnostics, and emits the login name only when no errors are
    present.
    
    A bx_logname_options struct and bx_logname_parse_options function were
    added, main was switched to a diag context, direct printf and exit calls
    were removed, and bx_cli_print_try_help is now invoked.

--- src/applets/base/logname/logname.c
+++ src/applets/base/logname/logname.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
-#include <unistd.h>
 #include <getopt.h>
 #include <stdbool.h>
-#include <string.h>
-#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #include "applets.h"
 #include "bx/diag.h"
-#include "bx/libbx.h"
 #include "lib/cli_common.h"
 
 static void bx_logname_print_help(FILE* stream, const char* progname) {
@@ -18,35 +15,87 @@ static void bx_logname_print_help(FILE* stream, const char* progname) {
     fprintf(stream, "      --version  output version information and exit\n");
 }
 
-int bx_logname_main(int argc, char** argv) {
+struct bx_logname_options {
+    const char* progname;
+    bool show_help;
+    bool show_version;
+};
+
+static bool bx_logname_parse_options(int argc,
+                                     char** argv,
+                                     struct bx_logname_options* options,
+                                     int* first_operand,
+                                     struct bx_diag_ctx* diag) {
     static const struct option long_options[] = {
         {"help", no_argument, NULL, 1},
         {"version", no_argument, NULL, 2},
         {NULL, 0, NULL, 0},
     };
-
     int c;
-    while ((c = getopt_long(argc, argv, "", long_options, NULL)) != -1) {
+
+    options->progname = bx_cli_progname((argc > 0) ? argv[0] : NULL, "logname");
+    options->show_help = false;
+    options->show_version = false;
+    diag->progname = options->progname;
+
+    opterr = 0;
+    optind = 1;
+
+    while ((c = getopt_long(argc, argv, "+", long_options, NULL)) != -1) {
         switch (c) {
             case 1:
-                bx_logname_print_help(stdout, "logname");
-                return 0;
+                options->show_help = true;
+                return true;
             case 2:
-                bx_cli_print_version("logname");
-                return 0;
+                options->show_version = true;
+                return true;
+            case '?':
+                bx_cli_diag_unrecognized_option(diag, optopt, optind, argc, argv);
+                return false;
             default:
-                return 1;
+                return false;
         }
     }
 
-    if (optind < argc) {
-        fprintf(stderr, "logname: extra operand '%s'\n", argv[optind]);
-        return 1;
+    *first_operand = optind;
+    return true;
+}
+
+int bx_logname_main(int argc, char** argv) {
+    struct bx_logname_options options;
+    struct bx_diag_ctx diag = {
+        .progname = "logname",
+        .exit_status = 0,
+        .verbose = false,
+        .debug = false,
+    };
+    int first_operand = 0;
+
+    if (!bx_logname_parse_options(argc, argv, &options, &first_operand, &diag)) {
+        bx_cli_print_try_help(diag.progname);
+        return diag.exit_status != 0 ? diag.exit_status : 1;
     }
 
-    char* login = getlogin();
-    if (!login) {
-        bx_pfatal(1, "no login name");
+    if (options.show_help) {
+        bx_logname_print_help(stdout, options.progname);
+        return 0;
+    }
+
+    if (options.show_version) {
+        bx_cli_print_version(options.progname);
+        return 0;
+    }
+
+    if (first_operand < argc) {
+        bx_cli_diag_extra_operand(&diag, argv[first_operand]);
+        bx_cli_print_try_help(options.progname);
+        return diag.exit_status != 0 ? diag.exit_status : 1;
+    }
+
+    const char* login = getlogin();
+    if (login == NULL) {
+        bx_diag(&diag, "no login name");
+        return diag.exit_status != 0 ? diag.exit_status : 1;
     }
     printf("%s\n", login);
 
