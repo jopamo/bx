32ae5c464b195d58d6a62de7eaca23df50589d40
Paul Moses <p@1g4.org>
2026-06-02 18:41:16 -0500

    upstream/src/jv_aux.c: validate sorted paths before processing
    Sorting paths may return an invalid jv. On that result the original
    object is freed and the error is returned instead of further processing.

--- src/applets/text/jq/upstream/src/jv_aux.c
+++ src/applets/text/jq/upstream/src/jv_aux.c
@@ -526,6 +526,10 @@ jv jv_delpaths(jv object, jv paths) {
     return jv_invalid_with_msg(jv_string("Paths must be specified as an array"));
   }
   paths = jv_sort(paths, jv_copy(paths));
+  if (!jv_is_valid(paths)) {
+    jv_free(object);
+    return paths;
+  }
   jv_array_foreach(paths, i, elem) {
     if (jv_get_kind(elem) != JV_KIND_ARRAY) {
       jv_free(object);
